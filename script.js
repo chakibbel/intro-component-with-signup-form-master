d922c36c4efb554d3e330f0d10f2afcc8213258e
Chakib B <chkwebdev@gmail.com>
2021-04-09 21:54:43 +0100

    style finalized

---
+++ script.js
@@ -0,0 +1,10 @@
+
+window.onload = function() {
+    var myBtn = document.getElementById("btn");
+    myBtn.addEventListener('click', myFunction);
+
+    function myFunction() {
+    alert ("button clicked");
+    } 
+
+} 
